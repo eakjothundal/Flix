3e0278b53ec88f52596709b2ba870232fd075b9e
Eakjot Hundal <eakjothundal@gmail.com>
2022-09-19 23:11:00 -0700

    Configured Superhero tab

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Eakjot Hundal on 9/19/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+}
